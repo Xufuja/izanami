19a759f5eb60de34091e59cb6bf5532e3b8cc3ae
Xufuja <xufuja@gmail.com>
2023-02-24 22:08:28 +0100

    Removing src directory from aggregator project

--- mmd-editor/src/main/java/dev/xfj/Main.java
+++ mmd-editor/src/main/java/dev/xfj/Main.java
@@ -1,7 +1,10 @@
 package dev.xfj;
 
+import dev.xfj.format.pmm.PMMFile;
 import dev.xfj.format.pmx.PMXFile;
+import dev.xfj.parsing.PMMParser;
 import dev.xfj.parsing.PMXParser;
+import dev.xfj.writer.PMMWriter;
 import dev.xfj.writer.PMXWriter;
 
 import java.io.IOException;
@@ -9,6 +12,15 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        PMMParser pmmParser = new PMMParser(Path.of("test_in_01.pmm"));
+        PMMFile pmmFile = pmmParser.parse();
+        PMMWriter pmmWriter = new PMMWriter(pmmFile, true);
+        pmmWriter.write(Path.of("test.pmm"));
+        int pmmFileSizeIn = pmmWriter.getPmmFileSizeIn();
+        int pmmFileSizeOut = pmmWriter.getPmmFileSizeOut();
+        System.out.println(String.format("Read:  %1$s bytes from file", pmmFileSizeIn));
+        System.out.println(String.format("Wrote: %1$s bytes to buffer", pmmFileSizeOut));
+
         PMXParser pmxParser = new PMXParser(Path.of("test_in_01.pmx"));
         PMXFile pmxFile = pmxParser.parse();
         PMXWriter pmxWriter = new PMXWriter(pmxFile, true);
--- src/main/java/dev/xfj/Main.java
+++
@@ -1,7 +0,0 @@
-package dev.xfj;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
