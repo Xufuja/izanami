80c2c2c93247476cf95d6cd8a6de5817b96d9f04
Xufuja <xufuja@gmail.com>
2023-04-19 21:30:03 +0200

    Added Properties panel

--- mmd-editor/src/main/java/dev/xfj/panels/SceneHierarchyPanel.java
+++ mmd-editor/src/main/java/dev/xfj/panels/SceneHierarchyPanel.java
@@ -3,8 +3,11 @@ package dev.xfj.panels;
 import dev.xfj.engine.scene.Entity;
 import dev.xfj.engine.scene.Scene;
 import dev.xfj.engine.scene.components.TagComponent;
+import dev.xfj.engine.scene.components.TransformComponent;
 import imgui.ImGui;
 import imgui.flag.ImGuiTreeNodeFlags;
+import imgui.type.ImString;
+import org.joml.Vector4f;
 
 public class SceneHierarchyPanel {
     private Scene context;
@@ -29,6 +32,14 @@ public class SceneHierarchyPanel {
                     Entity entity = new Entity(result.entity(), context);
                     drawEntityNode(entity);
                 });
+        if (ImGui.isMouseDown(0) && ImGui.isWindowHovered()) {
+            selectionContext = null;
+        }
+        ImGui.end();
+        ImGui.begin("Properties");
+        if (selectionContext != null) {
+            drawComponents(selectionContext);
+        }
         ImGui.end();
     }
 
@@ -47,6 +58,28 @@ public class SceneHierarchyPanel {
                 ImGui.treePop();
             }
             ImGui.treePop();
+        }
+    }
+
+    private void drawComponents(Entity entity) {
+        if (entity.hasComponent(TagComponent.class)) {
+            TagComponent tagComponent = entity.getComponent(TagComponent.class);
+            ImString buffer = new ImString(tagComponent.tag, 256);
+            if (ImGui.inputText("Tag", buffer)) {
+                tagComponent.tag = buffer.toString();
+            }
+        }
+
+        if (entity.hasComponent(TransformComponent.class)) {
+            TransformComponent transformComponent = entity.getComponent(TransformComponent.class);
+            if (ImGui.treeNodeEx(TransformComponent.class.hashCode(), ImGuiTreeNodeFlags.DefaultOpen, "Transform")) {
+                Vector4f transform = transformComponent.transform.getColumn(3, new Vector4f());
+                float[] newTransform = {transform.x, transform.y, transform.z};
+                ImGui.dragFloat3("Position", newTransform, 0.1f);
+                transformComponent.transform.setColumn(3, new Vector4f(newTransform[0], newTransform[1], newTransform[2], transform.w));
+
+                ImGui.treePop();
+            }
         }
     }
 }
