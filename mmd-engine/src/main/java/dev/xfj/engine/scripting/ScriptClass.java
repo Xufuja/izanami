183c07bcd2f86cf408592eab3807ea9b59e2c72b
Xufuja <xufuja@gmail.com>
2023-06-18 22:25:52 +0200

    Figuring out how to do the ScriptClass stuff

--- mmd-engine/src/main/java/dev/xfj/engine/scripting/ScriptClass.java
+++ mmd-engine/src/main/java/dev/xfj/engine/scripting/ScriptClass.java
@@ -1,4 +1,9 @@
 package dev.xfj.engine.scripting;
 
 public class ScriptClass {
+    private String className;
+    public ScriptClass(String className) {
+        this.className = className;
+    }
+    
 }
