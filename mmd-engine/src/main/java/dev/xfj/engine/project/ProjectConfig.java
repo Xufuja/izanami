37547548ece1f0d7f1765f96b45509a7b77b78fb
Xufuja <xufuja@gmail.com>
2023-07-30 21:37:03 +0200

    Removed placeholder values

--- mmd-engine/src/main/java/dev/xfj/engine/project/ProjectConfig.java
+++ mmd-engine/src/main/java/dev/xfj/engine/project/ProjectConfig.java
@@ -3,8 +3,8 @@ package dev.xfj.engine.project;
 import java.nio.file.Path;
 
 public class ProjectConfig {
-    public String name = "Sandbox";
-    public Path startScene = Path.of("scenes/Physics2D.scene");
-    public Path assetDirectory = Path.of("assets");
-    public Path scriptModulePath = Path.of("scripts/dist/sandbox.mjs");
+    public String name = "Untitled";
+    public Path startScene = Path.of("");
+    public Path assetDirectory = Path.of("");
+    public Path scriptModulePath = Path.of("");
 }
